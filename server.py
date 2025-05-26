e82769413b5981c4227e1bf5135c522d4a9d5e0e
tejasvadhyani <tejasvadhyani@gmail.com>
2025-05-26 23:28:30 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -11,8 +11,8 @@ def add_numbers(a: int, b: int) -> int:
 if __name__ == "__main__":
     # Streamable HTTP is the modern, production transport
     mcp.run(
-        transport="streamable-http",      # or "sse" if you really need SSE
+        transport="sse",      # or "sse" if you really need SSE
         host="0.0.0.0",
         port=int(os.getenv("PORT", 8000)),  # Render/Heroku inject $PORT
-        path="/mcp"                       # default; change only if you want /api etc.
+        path="/sse"                       # default; change only if you want /api etc.
     )
